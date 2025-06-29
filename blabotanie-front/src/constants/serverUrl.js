45a3d88e1893680d31891ce50d9e9df7b3058280
Roman Mezhuev <miffle1337@mail.ru>
2025-06-29 16:31:37 +0300

    Убрал старый фронт

--- blabotanie-front/src/constants/serverUrl.js
+++ blabotanie-front/src/constants/serverUrl.js
@@ -1,2 +1,3 @@
-export const API_URL = 'http://193.233.113.180:8087';
+// export const API_URL = 'http://193.233.113.180:8087';
+export const API_URL = 'http://localhost:8087';
 export const WS_URL = `${API_URL}/ws`;
--- frontend/src/pages/CallsPage.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import CallHistory from "../components/CallHistory";
-import "../styles/call-history.css";
-
-export default function CallsPage() {
-  return <CallHistory />;
-}
